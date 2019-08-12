cb70e4341516d8b19ff4597228ab354273be225b
Kevin Tanner <kevindavidtanner@gmail.com>
2019-08-12 14:35:57 -0600

    sets up the post model object, Step One - Model Objects

---
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Resources/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  Post
+//
+//  Created by Kevin Tanner on 8/12/19.
+//  Copyright © 2019 DevMtnStudent. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    
+    var text: String
+    var timestamp: TimeInterval
+    var username: String
+    
+    init(username: String, text: String, timestamp: TimeInterval = Date().timeIntervalSince1970) {
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp
+    }
+}
