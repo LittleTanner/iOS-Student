be244ce29455d137e025c468807ca3fd7962965d
Kevin Tanner <kevindavidtanner@gmail.com>
2019-08-12 14:46:45 -0600

    set up initial part of model controller , step two

---
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/Model Controllers/PostController.swift
@@ -0,0 +1,21 @@
+//
+//  PostController.swift
+//  Post
+//
+//  Created by Kevin Tanner on 8/12/19.
+//  Copyright © 2019 DevMtnStudent. All rights reserved.
+//
+
+import Foundation
+
+class PostController {
+    
+    let baseURL = URL(string: "https://devmtn-posts.firebaseio.com/posts")
+    
+    let posts: [Post] = []
+    
+    func fetchPosts(completion: @escaping (Post?) -> Void {
+        
+        })
+    
+}
