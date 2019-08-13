78994e8b93758b845fe13bfd2c182c292b215d69
Kevin Tanner <kevindavidtanner@gmail.com>
2019-08-13 16:10:01 -0600

    added posting functionality to the user interface, step two

--- Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/View Controllers/PostListViewController.swift
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/View Controllers/PostListViewController.swift
@@ -10,18 +10,18 @@ import UIKit
 
 class PostListViewController: UIViewController {
     
-
+    
     // MARK: - Outlets
     @IBOutlet weak var tableViewOutlet: UITableView!
     
     // MARK: - Properties
     var postController = PostController()
-
+    
     var refreshControl = UIRefreshControl()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         tableViewOutlet.delegate = self
         tableViewOutlet.dataSource = self
         
@@ -34,9 +34,16 @@ class PostListViewController: UIViewController {
         
         tableViewOutlet.refreshControl = refreshControl
         
-
+        
     }
-
+    
+    // MARK: - Actions
+    @IBAction func addNewPostButtonTapped(_ sender: Any) {
+        presentNewPostAlert()
+    }
+    
+    
+    
     // MARK: - Custom Methods
     @objc func refreshControlPulled() {
         refreshControl.addTarget(self, action: #selector(refreshControlPulled), for: .valueChanged)
@@ -56,18 +63,64 @@ class PostListViewController: UIViewController {
             self.tableViewOutlet.reloadData()
         }
     }
+    
+    func presentNewPostAlert() {
+        
+        let alertController = UIAlertController(title: "title", message: "message", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "usernameTextField"
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "messageTextField"
+        }
+        
+//        guard let usernameTextField = alertController.textFields?[0].text, let messageTextField = alertController.textFields?[1].text else { return }
+        
+//        guard (alertController.textFields?[0]) != nil, (alertController.textFields?[1] != nil) else { return }
+        
+        
+        alertController.addAction(UIAlertAction(title: "Post", style: .default, handler: { (UIAlertAction) in
+            
+            guard let usernameTextField = alertController.textFields?[0].text,
+//                let usernameInputText =
+                !usernameTextField.isEmpty,
+                let messageTextField = alertController.textFields?[1].text,
+                !messageTextField.isEmpty else {
+                    
+                    self.presentErrorAlert()
+                    return
+            }
+            
+            self.postController.addNewPostWith(username: usernameTextField, text: messageTextField, completion: {
+                    self.reloadTableView()
+            })
+            
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
 
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func presentErrorAlert() {
+        let alertController = UIAlertController(title: "Missing Info", message: "Try again", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 } // End of Class
 
 extension PostListViewController: UITableViewDelegate, UITableViewDataSource {
